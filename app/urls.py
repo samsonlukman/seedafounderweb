73ea625530ef830bb0ae56b3ae716956e59ad318
samsonlukman <lucassamson24@gmail.com>
2024-09-24 15:49:36 +0100

    designed index and other pages

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('founder/', views.founder, name='founder'),
     path('investor', views.investor, name='investor'),
-    path('/experts', views.experts, name='experts')
+    path('experts', views.experts, name='experts')
 ]
