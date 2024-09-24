54906a8dcc382b7920508fb770c9351f91f35e2d
samsonlukman <lucassamson24@gmail.com>
2024-09-24 13:34:55 +0100

    First interactive update

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('founder/', views.founder, name='founder'),
+    path('investor', views.investor, name='investor'),
+    path('/experts', views.experts, name='experts')
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def founder(request):
+    return render(request, 'founder.html')
+
+def investor(request):
+    return render(request, 'investor.html')
+
+def experts(request):
+    return render(request, 'experts.html')
