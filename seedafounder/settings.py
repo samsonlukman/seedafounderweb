4e3e9af4f292cfeb5458f8d9f38b7e1d30cb1402
samsonlukman <lucassamson24@gmail.com>
2024-09-24 10:51:10 +0100

    pushed navbar

--- seedafounder/settings.py
+++ seedafounder/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q33u$c0+9&9o1df479cd69an83ktmxs)4xjtoydoqh6$%+%$zo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['seedafounder.pythonanywhere.com']
 
 
 # Application definition
